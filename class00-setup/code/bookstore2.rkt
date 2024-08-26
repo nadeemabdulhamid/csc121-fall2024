5070208c6592e45986a9df6f7a8bbfad5ca4904b
Nadeem Abdul Hamid <nadeem@acm.org>
2024-08-26 08:52:47 -0400

    update dates

--- class00-setup/code/bookstore2.rkt
+++ class00-setup/code/bookstore2.rkt
@@ -1,8 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname bookstore2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
-;; CSC121 Spring 2011
-;; Lecture/Lab 1: January 13, 2011
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname bookstore2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; CSC121 Fall 2024
 ;; bookstore2.rkt
 
 ;; A Book is (make-book String Author Num Symbol)
