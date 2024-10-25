ec8e76be3909eb5c4ec2c1a7a3b4a8f3de8adeee
Nadeem Abdul Hamid <nadeem@acm.org>
2024-10-25 07:40:18 -0400

    Update BooksTest2.java

--- class13-mutation/code/BooksTest2.java
+++ class13-mutation/code/BooksTest2.java
@@ -11,28 +11,28 @@ public class BooksTest2 {
 	Book taocp2 =
 			new Book("The Art of Computer Programming (volume 2)", 129, 3, knuth);
 	
-	Author wings = new Author("J", "W", 1975);
-	Book bk2 =
-		    new Book("Comp Think", 120, 3, this.wings);
+	Author tardos = new Author("Eva", "Tardos", 1975);
+	Book algs =
+		    new Book("Algorithm Design", 120, 3, this.tardos);
 
 	@Test
 	public void testAuths() {
 		assertEquals( true,  this.knuth.books.contains(this.taocp1) );
 		assertEquals( true,  this.knuth.books.contains(this.taocp2) );
-		assertEquals( false,  this.knuth.books.contains(this.bk2) );
+		assertEquals( false,  this.knuth.books.contains(this.algs) );
 		
 		assertEquals( true,  this.taocp1.author.books.contains(this.taocp1) );
 		assertEquals( true,  this.taocp1.author.books.contains(this.taocp2) );
 		assertEquals( true,  this.taocp2.author.books.contains(this.taocp2) );
-		assertEquals( false,  this.taocp1.author.books.contains(this.bk2) );
+		assertEquals( false,  this.taocp1.author.books.contains(this.algs) );
 		
 		assertEquals( 2, this.knuth.books.length() );
 		this.knuth.addBook(taocp1);
 		assertEquals( 2, this.knuth.books.length() );
 		
-		assertEquals( 1, this.wings.books.length() );
-		assertThrows(RuntimeException.class, () -> this.wings.addBook(taocp2));
-		assertEquals( 1, this.wings.books.length() );
+		assertEquals( 1, this.tardos.books.length() );
+		assertThrows(RuntimeException.class, () -> this.tardos.addBook(taocp2));
+		assertEquals( 1, this.tardos.books.length() );
 
 	}
 
